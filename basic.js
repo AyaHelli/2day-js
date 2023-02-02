34b49b865ae7783d85d74687a3babca8398a7290
ericmenavi <ericmenavi@icloud.com>
2023-02-02 12:56:50 +0100

    second

--- basic.js
+++ basic.js
@@ -1,8 +1,11 @@
-let temp= Math.floor(Math.random()*30)-5;
-console.log(temp);
-if (temp<11){
-    console.log("The weather is Cold")
-}
-else {
-    console.log("the weather is Moderate")
-}
+// let temp= Math.floor(Math.random()*30)-5;
+// console.log(temp);
+// if (temp<11){
+//     console.log("The weather is Cold")
+// }
+// else {
+//     console.log("the weather is Moderate")
+// }
+let random= Math.floor(Math.random()*7);
+let favoriteFoods=["pizza", "hamburger", "ice cream" , "chocolate"];
+console.log(favoriteFoods[random]);
