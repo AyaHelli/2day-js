2a9b287f20cc6f5ca72015fbd354c26cf46b8525
ericmenavi <ericmenavi@icloud.com>
2023-02-02 13:15:12 +0100

    exercise 3

--- basic.js
+++ basic.js
@@ -6,6 +6,12 @@
 // else {
 //     console.log("the weather is Moderate")
 // }
-let random= Math.floor(Math.random()*7);
-let favoriteFoods=["pizza", "hamburger", "ice cream" , "chocolate"];
-console.log(favoriteFoods[random]);
+// let random= Math.floor(Math.random()*7);
+// let favoriteFoods=["pizza", "hamburger", "ice cream" , "chocolate"];
+// console.log(favoriteFoods[random]);
+
+function crystalGrazer(nchild,pname,geo,jobtittle){
+    document.write(`you will be a ${jobtittle} in ${geo} and married to ${pname} with ${nchild} children`);
+
+}
+crystalGrazer(2,"Potter","Austria","Manager");
